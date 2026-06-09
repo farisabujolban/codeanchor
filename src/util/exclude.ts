443ba51326e4c7d4c155e2ffeb629b065e09241d
farisabujolban <faris.abujolban@gmail.com>
2026-06-09 09:29:19 -0700

    style: fix prettier formatting

--- src/util/exclude.ts
+++ src/util/exclude.ts
@@ -1,15 +1,15 @@
 function matchesGlob(filePath: string, pattern: string): boolean {
-  const regex = new RegExp(
-    '^' +
-      pattern
-        .replace(/\./g, '\\.')
-        .replace(/\*\*/g, '(.+)')
-        .replace(/(?<!\()\*/g, '[^/]+') +
-      '$',
-  )
-  return regex.test(filePath)
+    const regex = new RegExp(
+        '^' +
+            pattern
+                .replace(/\./g, '\\.')
+                .replace(/\*\*/g, '(.+)')
+                .replace(/(?<!\()\*/g, '[^/]+') +
+            '$',
+    );
+    return regex.test(filePath);
 }
 
 export function isExcluded(filePath: string, exclude: string[]): boolean {
-  return exclude.some(pattern => matchesGlob(filePath, pattern))
+    return exclude.some((pattern) => matchesGlob(filePath, pattern));
 }
--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,5 +1,5 @@
-import { createHash } from 'node:crypto'
+import { createHash } from 'node:crypto';
 
 export function sha256(input: string): string {
-  return createHash('sha256').update(input, 'utf-8').digest('hex')
+    return createHash('sha256').update(input, 'utf-8').digest('hex');
 }
